3e8ce6701aa341213f778798edcf3a5bf2a7452b
Oscar Garcia <ogarciamasia@gmail.com>
2024-01-19 23:16:55 +0100

    updates with stencil flag

--- stencil.config.ts
+++ stencil.config.ts
@@ -7,6 +7,7 @@ export const config: Config = {
     experimentalImportInjection: true,
     experimentalSlotFixes: true,
     scopedSlotTextContentFix: true,
+    experimentalScopedSlotChanges: true,
   },
   outputTargets: [
     {
